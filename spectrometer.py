9eee079ad1c5c95155850bbbc042eca7148b76ec
Elliot-Wadge <75158330+Elliot-Wadge@users.noreply.github.com>
2021-05-16 19:55:58 -0700

    made the ui an import so saves are not changed when using qt-designer

--- spectrometer.py
+++ spectrometer.py
@@ -2,9 +2,15 @@ import numpy as np
 
 
 class Spectrometer():
-    def __init__(wavelength):
+    def __init__():
 
-        self._position = wavelength
+        f = open('last_position', 'r')
+        try:
+            self.position = float(f.readline())
+        except:
+            print("there was an error trying to load the last position from 'last_position'")
+
+        f.close()
 
     @property
     def position(self):
@@ -19,6 +25,7 @@ class Spectrometer():
             print('not valid entry')
         return
 
+
     def recalibrate(self,wavelength):
         self.position = Wavelength
 
@@ -30,6 +37,6 @@ class Spectrometer():
         pass
 
     def close(self):
-        f = open(last_position, 'w')
+        f = open('last_position', 'w')
         f.write(self.position)
         f.close()
