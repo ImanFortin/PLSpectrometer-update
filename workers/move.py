68b53f67446c2b4e567f2614f31704a0d3a3c499
Elliot <ewadge@sfu.ca>
2021-05-22 20:48:44 -0700

    the two worker functions to do the scan and the move

---
+++ workers/move.py
@@ -0,0 +1,33 @@
+import time
+from PyQt5.QtCore import QObject, QThread, pyqtSignal
+
+#this is the move worker he does the moving
+class moveWorker(QObject):
+
+    progress = pyqtSignal(list)
+    position = pyqtSignal(float)
+    finished = pyqtSignal()
+
+
+    def __init__(self,spectrometer,end):
+        super().__init__()
+        self.spectrometer = spectrometer
+        self.end = end
+
+    def move(self):
+
+        start = int(self.spectrometer.position)#get the current position
+        end = int(self.end)#get the end position
+        distance = abs(end - start)#get the distance
+        direction = int((end - start)/distance)#get the direction
+        self.progress.emit([0, abs(start - end)])#set progress to zero
+
+        print(start, end + direction, direction)
+        for i in range(start, end, direction):
+            time.sleep(1)#simulate the move
+            self.progress.emit([abs(i-start),abs(start - end)]) #emit the progress
+            self.position.emit(i) #emit the position
+
+
+        self.progress.emit([abs(start - end), abs(start - end)])#emit that progress is done
+        self.finished.emit()#emit that we're done
---
+++ workers/scan.py
@@ -0,0 +1,62 @@
+import time
+from PyQt5.QtCore import QObject, QThread, pyqtSignal
+
+#this is the scan worker he does the scanning
+class scanWorker(QObject):
+
+    #this is all the information we will be sending to UI while we run
+    data = pyqtSignal(float)
+    progress = pyqtSignal(list)
+    position = pyqtSignal(float)
+    finished = pyqtSignal()
+
+
+    def __init__(self,UI,spectrometer,start,end,step,time):
+        super().__init__()
+        self.spectrometer = spectrometer
+        self.start = start
+        self.end = end
+        self.step = step
+        self.time = time
+
+    def scan(self):
+
+        #first copy the move function above to move to the start
+        start = int(self.spectrometer.position)
+        end = int(self.start)#we want to move to the start point
+        distance = abs(end - start)
+        direction = int((end - start)/distance)
+        print(start,end,direction)
+
+        # self.progress.emit([0, abs(start - end)])
+        for i in range(start, end + direction, direction):
+
+
+
+            print(i)
+            self.position.emit(i) #emit the position
+            time.sleep(1)#simulate the move collection
+
+
+        #set the new start to be the current position
+        start = int(self.spectrometer.position)
+        end = int(self.end) #set end to the end position of the scan
+
+        distance = abs(end - start)
+        direction = int((end - start)/distance)
+        print(start,end,direction)
+
+        for i in range(start, end + direction, direction):
+
+            
+
+            print(i)
+            self.position.emit(i) #emit the position
+
+            self.data.emit(i/2)
+
+            time.sleep(1) #simulate collecting data for one second
+            self.progress.emit([abs(i-start),abs(start - end)])
+
+        self.progress.emit([abs(start - end), abs(start - end)])
+        self.finished.emit()#emit that we're done
