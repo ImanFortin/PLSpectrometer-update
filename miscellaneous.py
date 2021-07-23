aa100d2a71c58a6af89b597e8e252feed47008b6
Elliot Wadge <ewadge@sfu.ca>
2021-07-23 14:53:50 -0700

    fixed the new filename for double digit numbers and above

--- miscellaneous.py
+++ miscellaneous.py
@@ -11,29 +11,30 @@ def sleep(duration, get_now=time.perf_counter):
 #avoid overwriting data
 def available_name(filename):
 
-    exists = os.path.exists(filename)
-
-    depth =  0
-
-    while exists:
-        dot = filename.find('.')
-         #if there is no postfix set dot to zero
-        if dot == -1:#if there is no dot do this
-            if depth == 0:
-                filename = filename + ' (1)'
-
-            else:
-                filename = filename[:-2] + str(depth + 1) + ')'
-        else:#If there is a dot do this
-            if depth == 0:
-                filename = filename[:dot] + '(' + str(depth + 1) + ')' + filename[dot:] #add the brackets and number before the dot
-            else:
-                filename = filename[:dot - 2] + str(depth + 1) + filename[dot - 1:]#replace the number
-
+    '''checks if the filename is available and returns the next best name'''
         exists = os.path.exists(filename)
-        depth += 1
 
-    return filename
+        depth =  0
+
+        while exists:
+            dot = filename.find('.')
+            if dot == -1:#if there is no dot do this
+                if depth == 0:
+                    filename = filename + ' (1)'
+
+                else:
+                    filename = filename[:filename.rfind('(')+1] + str(depth + 1) + ')'
+                    print(filename)
+            else:#If there is a dot do this
+                if depth == 0:
+                    filename = filename[:dot] + '(' + str(depth + 1) + ')' + filename[dot:] #add the brackets and number before the dot
+                else:
+                    filename = filename[:dot - 2] + str(depth + 1) + filename[dot - 1:]#replace the number
+
+            exists = os.path.exists(filename)
+            depth += 1
+
+        return filename
 
 #function to make header it takes f as in f = open(filename, 'w'), the smaple id and the count time
 def make_header(f, sample_id, time_avg):
