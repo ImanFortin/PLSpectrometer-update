568889179d1efdabb9e2ed8398e5f83c766111a6
Elliot Wadge <ewadge@sfu.ca>
2021-07-23 16:02:09 -0700

    fixed indent error

--- miscellaneous.py
+++ miscellaneous.py
@@ -10,31 +10,30 @@ def sleep(duration, get_now=time.perf_counter):
 
 #avoid overwriting data
 def available_name(filename):
-
     '''checks if the filename is available and returns the next best name'''
+    exists = os.path.exists(filename)
+
+    depth =  0
+
+    while exists:
+        dot = filename.find('.')
+        if dot == -1:#if there is no dot do this
+            if depth == 0:
+                filename = filename + ' (1)'
+
+            else:
+                filename = filename[:filename.rfind('(')+1] + str(depth + 1) + ')'
+                print(filename)
+        else:#If there is a dot do this
+            if depth == 0:
+                filename = filename[:dot] + '(' + str(depth + 1) + ')' + filename[dot:] #add the brackets and number before the dot
+            else:
+                filename = filename[:dot - 2] + str(depth + 1) + filename[dot - 1:]#replace the number
+
         exists = os.path.exists(filename)
+        depth += 1
 
-        depth =  0
-
-        while exists:
-            dot = filename.find('.')
-            if dot == -1:#if there is no dot do this
-                if depth == 0:
-                    filename = filename + ' (1)'
-
-                else:
-                    filename = filename[:filename.rfind('(')+1] + str(depth + 1) + ')'
-                    print(filename)
-            else:#If there is a dot do this
-                if depth == 0:
-                    filename = filename[:dot] + '(' + str(depth + 1) + ')' + filename[dot:] #add the brackets and number before the dot
-                else:
-                    filename = filename[:dot - 2] + str(depth + 1) + filename[dot - 1:]#replace the number
-
-            exists = os.path.exists(filename)
-            depth += 1
-
-        return filename
+    return filename
 
 #function to make header it takes f as in f = open(filename, 'w'), the smaple id and the count time
 def make_header(f, sample_id, time_avg):
