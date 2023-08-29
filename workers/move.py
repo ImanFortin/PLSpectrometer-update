2b8b1b945110be8a95fa5cfcf5da938a4684a7da
Alistair Bevan <trexbevan@gmail.com>
2023-08-29 10:42:59 -0700

    Format cleanup

--- workers/move.py
+++ workers/move.py
@@ -1,25 +1,32 @@
+# move.py
+#
+# Move worker which does all the moving
+# Created by Elliot Wadge
+# Edited by Alistair Bevan
+# August 2023
+#
+
 import time
 from PyQt5.QtCore import QObject, QThread, pyqtSignal
 
-#this is the move worker he does the moving
+
 class moveWorker(QObject):
 
-    #these are the signals that are connected to functions in GUI.py
+    # These are the signals that are connected to functions in GUI.py
     position = pyqtSignal(float)
     finished = pyqtSignal()
 
-    #to initialize we need to take the spectrometer that is being moveToThread
-    def __init__(self,spectrometer,end):
-        super().__init__() #this just calls the parent init method always required if making a worker
-        self.spectrometer = spectrometer #will be either single or double
-        self.end = end #the desination
+    # To initialize we need to take the spectrometer that is being moveToThread
+    def __init__(self, spectrometer, end):
+        super().__init__()  # This just calls the parent init method and is always required if making a worker
+        self.spectrometer = spectrometer  # Will be either single or double
+        self.end = end  # The desination
         self.abort = False
 
     def move(self):
-
-        start = self.spectrometer.position#get the current position
-        end = self.end#get the end position
-        distance = round(abs(end - start),3)
+        start = self.spectrometer.position  # Get the current position
+        end = self.end  # Get the end position
+        distance = round(abs(end - start), 3)
 
         if end < start:
             direction = -1
@@ -31,17 +38,17 @@ class moveWorker(QObject):
 
         high = 1/(2*self.spectrometer.frequency)
         low = high
-        #set the direction voltage
-        self.spectrometer.set_direction(direction)#see spectrometer.py for the method
+        # Set the direction voltage
+        self.spectrometer.set_direction(direction)  # See spectrometer.py for the method
 
-        if direction < 0:#if the direction is backwards
-            self.spectrometer.move(distance + 20, high_time = high, low_time = low)#first move to twenty nm back
+        if direction < 0:  # If the direction is backwards
+            self.spectrometer.move(distance + 20, high_time = high, low_time = low)  # First move to 20 nm back from start
             direction = 1
-            self.spectrometer.set_direction(direction) #change directions
-            self.spectrometer.move(19.97, high_time = high, low_time = low) #move to within 0.03 nm of the positin
-            self.spectrometer.move(0.03, high_time = high, low_time = 0.25) #do the last 0.03 nm with 1s in between each step (4 pulses a step)
+            self.spectrometer.set_direction(direction)  # Change directions
+            self.spectrometer.move(19.97, high_time = high, low_time = low)  # Move to within 0.03 nm of the positin
+            self.spectrometer.move(0.03, high_time = high, low_time = 0.25)  # Do the last 0.03 nm with 1s in between each step (4 pulses a step)
 
-        elif direction >= 0:#otherwise just move forwards within 0.03 and then slow down
+        elif direction >= 0:  # Otherwise just move forwards within 0.03 and then slow down
             if distance < 0.03:
                 self.spectrometer.move(distance, high_time = high, low_time = 0.25)
             else:
@@ -49,8 +56,7 @@ class moveWorker(QObject):
                 self.spectrometer.move(0.03, high_time = high, low_time = 0.25)
 
         self.position.emit(end)
-        self.finished.emit()#emit that we're done
-
+        self.finished.emit()  # Emit that we're done
 
     def check_abort(self):
         if self.abort:
